b3d47b51d7a3a2684b1a9767ef7f5a6db91772ef
Theo <todorlankovszu@gmail.com>
2023-10-30 12:22:31 +0100

    dashboard und navbar kombiniert und funktionalit

--- lib/application/dashboard.dart
+++ lib/application/dashboard.dart
@@ -10,131 +10,129 @@ class Dashboard extends StatelessWidget {
   final _name = 'Theo';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: 130,
-              margin: const EdgeInsets.only(
-                top: 50,
-                left: 20,
-                right: 20,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Text(
-                'Good Day $_name!',
-                textAlign: TextAlign.center,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 27.5,
-                    fontStyle: FontStyle.normal),
-              ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: 130,
+            margin: const EdgeInsets.only(
+              top: 50,
+              left: 20,
+              right: 20,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      height: 150,
-                      width: 150,
-                      margin: const EdgeInsets.only(
-                        top: 20,
-                        left: 10,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: Text(
-                        'Good Day $_name!',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27.5,
-                            fontStyle: FontStyle.normal),
-                      ),
-                    ),
-                    Container(
-                      height: 150,
-                      width: 150,
-                      margin: const EdgeInsets.only(
-                        top: 20,
-                        left: 10,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: Text(
-                        'Good Day $_name!',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27.5,
-                            fontStyle: FontStyle.normal),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      height: 225,
-                      width: 150,
-                      margin: const EdgeInsets.only(
-                        top: 20,
-                        right: 10,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: Text(
-                        'Good Day $_name!',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27.5,
-                            fontStyle: FontStyle.normal),
-                      ),
-                    ),
-                    Container(
-                      height: 75,
-                      width: 150,
-                      margin: const EdgeInsets.only(
-                        top: 20,
-                        right: 10,
-                      ),
-                      decoration: const BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      child: Text(
-                        'Good Day $_name!',
-                        textAlign: TextAlign.center,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 27.5,
-                            fontStyle: FontStyle.normal),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            decoration: const BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
             ),
-          ],
-        ),
+            child: Text(
+              'Good Day $_name!',
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 27.5,
+                  fontStyle: FontStyle.normal),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 150,
+                    width: 150,
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      left: 10,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Text(
+                      'Good Day $_name!',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 27.5,
+                          fontStyle: FontStyle.normal),
+                    ),
+                  ),
+                  Container(
+                    height: 150,
+                    width: 150,
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      left: 10,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Text(
+                      'Good Day $_name!',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 27.5,
+                          fontStyle: FontStyle.normal),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    height: 225,
+                    width: 150,
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      right: 10,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Text(
+                      'Good Day $_name!',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 27.5,
+                          fontStyle: FontStyle.normal),
+                    ),
+                  ),
+                  Container(
+                    height: 75,
+                    width: 150,
+                    margin: const EdgeInsets.only(
+                      top: 20,
+                      right: 10,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    child: Text(
+                      'Good Day $_name!',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 27.5,
+                          fontStyle: FontStyle.normal),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
