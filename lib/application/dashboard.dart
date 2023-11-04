45d4089ac86640b8dd558ed82ccf1a39642b7807
Theo <todorlankovszu@gmail.com>
2023-11-04 19:21:36 +0100

    updates version of the last push removed 3 lines

--- lib/application/dashboard.dart
+++ lib/application/dashboard.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_linkify/flutter_linkify.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(Dashboard());
